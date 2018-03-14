17107514bd80781d88cd90718c122d4636a2fda6
wykys <vvykys@gmail.com>
2018-03-14 19:12:52 +0100

    code refactored

--- sequencer_v0.1/main.h
+++ sequencer_v0.1/main.h
@@ -61,12 +61,12 @@ void setup(void)
 
 void timer1_set_state(state_t state)    // switch, which turn on (1) timer1, or turn off (0)
 {
-    (state == ENABLE) ? TCCR1B |= (1<<CS12) : TCCR1B &= ~(1<<CS12);
+    (state == ENABLE) ? (TCCR1B |= (1<<CS12)) : (TCCR1B &= ~(1<<CS12));
 }
 
 void button_ptt_set_irq(state_t state)
 {
-    (state == ENABLE) ? EIMSK |= 1<<INT0 : EIMSK &= ~(1<<INT0);
+    (state == ENABLE) ? (EIMSK |= 1<<INT0) : (EIMSK &= ~(1<<INT0));
 }
 
 void way_up(void)
