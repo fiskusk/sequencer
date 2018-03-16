c21a9bf15265b0cd7fc6e09251a229bb0ee23fe7
wykys <vvykys@gmail.com>
2018-03-16 23:19:19 +0100

    updated events.h

--- sequencer_v0.1/events.h
+++ sequencer_v0.1/events.h
@@ -17,16 +17,16 @@
 
 
 // ========================================================
-extern void Event_PTT_pushed_up_oN_relay1_oN_FAN(void);
-extern void Event_PTT_pushed_down_oFF_Ucc(void);
-extern void error(void);
-extern void E0_on_off_relay2(void);
-extern void E1_on_off_bias(void);
-extern void E2_on_Ucc_off_relay1(void);
-extern void fault_off_all(void);
-extern void after_fault_check_status(void);
-extern void test_state_of_PTT_button(void);
-extern void Event_PTT_button_status_changed(void);
+void Event_PTT_pushed_up_oN_relay1_oN_FAN(void);
+void Event_PTT_pushed_down_oFF_Ucc(void);
+void error(void);
+void E0_on_off_relay2(void);
+void E1_on_off_bias(void);
+void E2_on_Ucc_off_relay1(void);
+void fault_off_all(void);
+void after_fault_check_status(void);
+void test_state_of_PTT_button(void);
+void Event_PTT_button_status_changed(void);
 // ========================================================
 
 #endif // EVENTS_H_INCLUDED
