ab8a7aa9eb6e8dc75752104f23bcb4d740cf2c80
wykys <vvykys@gmail.com>
2018-03-16 23:18:18 +0100

    updated permitters.h

--- sequencer_v0.1/permitters.h
+++ sequencer_v0.1/permitters.h
@@ -3,7 +3,7 @@
  *
  * Created: 16.3.2018 16:48:31
  *  Author: fkla
- */ 
+ */
 
 
 #ifndef PERMITTERS_H_INCLUDED
@@ -13,9 +13,9 @@
 #include <avr/io.h>
 
 // ========================================================
-extern	void timer1_set_state(state_t state);    // switch, which turn on (1) timer1, or turn off (0)
-extern	void button_ptt_set_irq(state_t state);
+void timer1_set_state(state_t state);    // switch, which turn on (1) timer1, or turn off (0)
+void button_ptt_set_irq(state_t state);
 // ========================================================
 
 
-#endif // PERMITTERS_H 
+#endif // PERMITTERS_H
