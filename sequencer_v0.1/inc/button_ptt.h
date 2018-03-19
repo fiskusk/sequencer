a941d592b9561c6fe1ca21ef373a74f3880d00a2
wykys <vvykys@gmail.com>
2018-03-19 23:08:03 +0100

    added button_ptt files

---
+++ sequencer_v0.1/inc/button_ptt.h
@@ -0,0 +1,16 @@
+#ifndef PTT_H_INCLUDED
+#define PTT_H_INCLUDED
+
+#include "settings.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "types.h"
+#include "main.h"
+#include "uart.h"
+#include "permitters.h"
+
+
+
+
+#endif // PTT_H_INCLUDED
---
+++ sequencer_v0.1/src/button_ptt.c
@@ -0,0 +1,1 @@
+#include "button_ptt.h"
