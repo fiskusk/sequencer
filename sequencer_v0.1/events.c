6a3148cb66bb0c265b0e3ef1dcd50ed2df90c1d8
wykys <vvykys@gmail.com>
2018-03-19 19:26:57 +0100

    c style revidion

--- sequencer_v0.1/events.c
+++ sequencer_v0.1/events.c
@@ -1,11 +1,11 @@
 #include "events.h"
 
 /*
-* Event for PTT is pushed up and device turn relay1
-* and FAN independently directly.
-* Next state is EVENT0 - turn relay2.
-* They should be turned on simultaneously with relay1.
-*/
+ * Event for PTT is pushed up and device turn relay1
+ * and FAN independently directly.
+ * Next state is EVENT0 - turn relay2.
+ * They should be turned on simultaneously with relay1.
+ */
 void event_PTT_pushed_up_on_relay1_on_FAN(void)
 {
     timer1_set_state(DISABLE);
@@ -20,9 +20,9 @@ void event_PTT_pushed_up_on_relay1_on_FAN(void)
 }
 
 /*
-* Event for PTT is pushed down and device turn off Ucc.
-* Next state is EVENT1 - (way = 0) turn off bias.
-*/
+ * Event for PTT is pushed down and device turn off Ucc.
+ * Next state is EVENT1 - (way = 0) turn off bias.
+ */
 void event_PTT_pushed_down_off_Ucc(void)
 {
     timer1_set_state(DISABLE);
@@ -37,8 +37,8 @@ void event_PTT_pushed_down_off_Ucc(void)
 }
 
 /*
-* If pressed or push down PTT and was fault, these function block
-*/
+ * If pressed or push down PTT and was fault, these function block
+ */
 void error(void)
 {
     timer1_set_state(DISABLE);
@@ -47,11 +47,11 @@ void error(void)
     uart_puts("Tlacis ale nezapnu se, mam poruchu\n");
     pom = "nastala chyba       ";
 
-    button_ptt_set_irq(DISABLE);        // deny next PTT interrupt
-    actual_state = FAULT;                // next EVENT will be FAULT
-    TCNT1 = 65520;                      // jump immediately o ISR_TIMER1
+    button_ptt_set_irq(DISABLE); // deny next PTT interrupt
+    actual_state = FAULT;        // next EVENT will be FAULT
+    TCNT1        = 65520;        // jump immediately o ISR_TIMER1
     timer1_set_state(ENABLE);
-   }
+}
 
 void E0_on_off_relay2(void)
 {
@@ -111,10 +111,10 @@ void E2_on_Ucc_off_relay1(void)
 }
 
 /*
-* In these function deciding, if button after 10ms sequence
-* was pressed/push off.
-* According to this, it calls the appropriate functions.
-*/
+ * In these function deciding, if button after 10ms sequence
+ * was pressed/push off.
+ * According to this, it calls the appropriate functions.
+ */
 void test_state_of_PTT_button(void)
 {
     timer1_set_state(DISABLE);
@@ -128,7 +128,7 @@ void test_state_of_PTT_button(void)
     // if button is pressed and at the same time was not fault or after_fault
     if (button_ptt_is_pressed() && actual_state != FAULT && actual_state != AFTER_FAULT)
     {
-        //test prints
+        // test prints
         uart_puts("zapinam\n");
 
         way = 1;
@@ -148,21 +148,21 @@ void test_state_of_PTT_button(void)
     {
         error();
     }
-}
+} /* test_state_of_PTT_button */
 
 /* In this function set TIMER1 to achieve 10ms delay
-*  after change status of button and set state TEST_PTT.
-* State TEST_PTT deciding, if button after three 10ms sequence
-* was pressed/push off or during these 30 seconds was button glitch.
-* According to this, it calls the appropriate functions.
-*/
+ *  after change status of button and set state TEST_PTT.
+ * State TEST_PTT deciding, if button after three 10ms sequence
+ * was pressed/push off or during these 30 seconds was button glitch.
+ * According to this, it calls the appropriate functions.
+ */
 void event_PTT_button_status_changed(void)
 {
     timer1_set_state(DISABLE);
     TCNT1 = 64910;
     if (once_PTT_event == 1)
     {
-        old_state = actual_state;
+        old_state      = actual_state;
         once_PTT_event = loop_repeat(DISABLE);
     }
     actual_state = TEST_PTT;
@@ -172,13 +172,13 @@ void event_PTT_button_status_changed(void)
 
 void fault_off_all(void)
 {
-    timer1_set_state(DISABLE);   // TIMER1 stops
-    button_ptt_set_irq(DISABLE); // ISR from PTT button disable, block transmit
-    if (once_fault_event == 1 && ((fault_flag == 1) || (fault_flag == 2)))                    // do this only once - turn all down if fault
+    timer1_set_state(DISABLE);                                             // TIMER1 stops
+    button_ptt_set_irq(DISABLE);                                           // ISR from PTT button disable, block transmit
+    if (once_fault_event == 1 && ((fault_flag == 1) || (fault_flag == 2))) // do this only once - turn all down if fault
     {
-        //PORTC ^= (1<<5);
+        // PORTC ^= (1<<5);
         uart_puts("nastal FAULT - FAULT postupne vse vypnu a drz?m delsi dobu, tedy tohle vse vcetne vypinani vseho delam znovu...\n");
-        fault_flag = 3;                 // set fault flag to after_fault state
+        fault_flag = 3; // set fault flag to after_fault state
         adc_set_state(ENABLE);
 
         // test prints
@@ -189,7 +189,7 @@ void fault_off_all(void)
         // number of repeats to achieve aim delay time about 20 seconds
         // if first run, fault_count set to short time delay
         fault_count++;
-        actual_state = FAULT;     // go to fault in ISR timer1
+        actual_state = FAULT; // go to fault in ISR timer1
 
         once_fault_event = loop_repeat(DISABLE);
         timer1_set_state(ENABLE); // set on timer1
@@ -200,7 +200,7 @@ void fault_off_all(void)
     {
         TCNT1 = TFAULT;
         fault_count++;
-        actual_state = FAULT;       // still hold here
+        actual_state = FAULT; // still hold here
 
         // test prints
         uart_puts("FAULT_count: ");
@@ -215,10 +215,10 @@ void fault_off_all(void)
         // test prints
         uart_puts("vse vypnuto, delay dosahnut. Nasleduje kontrola v AFTER_FAULT\n");
 
-        actual_state = AFTER_FAULT;     // go to test device
+        actual_state = AFTER_FAULT; // go to test device
 
         once_fault_event = loop_repeat(ENABLE);
-        TIFR1 |= 1<<TOV1;      // jump quickly to ISR Timer1 into AFTER_FAULT
+        TIFR1 |= 1 << TOV1;       // jump quickly to ISR Timer1 into AFTER_FAULT
         timer1_set_state(ENABLE); // run TIMER1
     }
 } /* fault_off_all */
@@ -227,12 +227,11 @@ void fault_off_all(void)
 void after_fault_check_status(void)
 {
     timer1_set_state(DISABLE);
-    if (fault_flag>0)
+    if (fault_flag > 0)
     {
         fault_flag = 1;
         uart_puts("Byla chyba, kontrola jestli je vse OK\n");
         pom = "SW&RD ADC a COMP    ";
-
     }
     else
     {
@@ -245,65 +244,66 @@ void after_fault_check_status(void)
 
 void processing_adc_data(void)
 {
-    if (fault_flag == 2)            // if first run after start up device, read ADC value as ADC_SWR
+    if (fault_flag == 2) // if first run after start up device, read ADC value as ADC_SWR
     {
         uart_puts("first start - copy ADC to ADC_SWR\n");
         ADC_SWR = ADC;
     }
-    if (((ADC_SWR<UMIN) || (ADC_SWR>UMAX)) && ((fault_flag == 0) || (fault_flag == 1)))
+    if (((ADC_SWR < UMIN) || (ADC_SWR > UMAX)) && ((fault_flag == 0) || (fault_flag == 1)))
     {
-        //uart_puts("ADC hodnota ");
-        //uart_puts(buffer4);
-        //uart_puts(" je mimo rozsah, generuji fault flag\n");
-        //PORTC ^= (1<<5);
+        // uart_puts("ADC hodnota ");
+        // uart_puts(buffer4);
+        // uart_puts(" je mimo rozsah, generuji fault flag\n");
+        // PORTC ^= (1<<5);
         actual_state = FAULT;
-        TIFR1 |= 1<<TOV1;
-        fault_flag = 1;
-        fault_count = 0;
+        TIFR1       |= 1 << TOV1;
+        fault_flag   = 1;
+        fault_count  = 0;
         timer1_set_state(ENABLE);
     }
     else if (fault_flag == 1)
     {
-        //uart_puts("hodnota ADC ");
-        //uart_puts(buffer4);
-        //uart_puts(" je OK, vracim fault_flag = 0\n");
+        // uart_puts("hodnota ADC ");
+        // uart_puts(buffer4);
+        // uart_puts(" je OK, vracim fault_flag = 0\n");
         actual_state = AFTER_FAULT;
-        fault_flag = 0;
-        TIFR1 |= 1<<TOV1;
+        fault_flag   = 0;
+        TIFR1       |= 1 << TOV1;
         timer1_set_state(ENABLE);
     }
     else if (fault_flag == 0 || fault_flag == 3)
     {
-        switch (adc_active_channel) {
-        case ADC_CHANNEL_SWR:
-            ADC_TEMP_INT = ADC;
-            adc_active_channel = ADC_CHANNEL_TEMP_HEATSINK;
-            break;
-        case ADC_CHANNEL_TEMP_HEATSINK:
-            ADC_SWR = ADC;
-            adc_active_channel = ADC_CHANNEL_POWER;
-            break;
-        case ADC_CHANNEL_POWER:
-            PORTC ^= (1<<5);
-            ADC_TEMP_HEATSINK = ADC;
-            adc_active_channel = ADC_CHANNEL_Ucc;
-            break;
-        case ADC_CHANNEL_Ucc:
-            ADC_POWER = ADC;
-            adc_active_channel = ADC_CHANNEL_Icc;
-            break;
-        case ADC_CHANNEL_Icc:
-            ADC_Ucc = ADC;
-            adc_active_channel = ADC_CHANNEL_TEMP_INT;
-            break;
-        case ADC_CHANNEL_TEMP_INT:
-            ADC_Icc = ADC;
-            adc_active_channel = ADC_CHANNEL_SWR;
-            break;
-        default:
-            adc_active_channel = ADC_CHANNEL_SWR;
-            break;
+        switch (adc_active_channel)
+        {
+            case ADC_CHANNEL_SWR:
+                ADC_TEMP_INT       = ADC;
+                adc_active_channel = ADC_CHANNEL_TEMP_HEATSINK;
+                break;
+            case ADC_CHANNEL_TEMP_HEATSINK:
+                ADC_SWR = ADC;
+                adc_active_channel = ADC_CHANNEL_POWER;
+                break;
+            case ADC_CHANNEL_POWER:
+                PORTC ^= (1 << 5);
+                ADC_TEMP_HEATSINK  = ADC;
+                adc_active_channel = ADC_CHANNEL_Ucc;
+                break;
+            case ADC_CHANNEL_Ucc:
+                ADC_POWER = ADC;
+                adc_active_channel = ADC_CHANNEL_Icc;
+                break;
+            case ADC_CHANNEL_Icc:
+                ADC_Ucc = ADC;
+                adc_active_channel = ADC_CHANNEL_TEMP_INT;
+                break;
+            case ADC_CHANNEL_TEMP_INT:
+                ADC_Icc = ADC;
+                adc_active_channel = ADC_CHANNEL_SWR;
+                break;
+            default:
+                adc_active_channel = ADC_CHANNEL_SWR;
+                break;
         }
     }
     ADMUX = ADMUX & 0xF0 | adc_active_channel;
-}
+} /* processing_adc_data */
